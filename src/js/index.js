4dfe25b3a9573e963b168931bba38729fd3a98a8
ojeanx <123332576+ojeanx@users.noreply.github.com>
2023-01-23 14:27:18 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const botaoTrailer = document.querySelector (".botao-trailer");
+const botaoFecharModal = document.querySelector (".fechar-modal");
+const video = document.getElementById ("video");
+const modal = document.querySelector (".modal");
+const linkDoVideo = video.src;
+
+function alternarmodal () {
+    modal.classList.toggle ("aberto");
+}
+
+botaoTrailer.addEventListener ("click", () => {
+    alternarmodal ();
+    video.setAttribute("src", linkDoVideo);
+})
+
+botaoFecharModal.addEventListener ("click", () => {
+    alternarmodal ();
+    video.setAttribute ("src", "" )
+})
+
+
+
